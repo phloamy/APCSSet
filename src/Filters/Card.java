7dbd76884df927611cc119b1411309f2e9b9cda7
salvadorio <38008394+salvadorio@users.noreply.github.com>
2022-02-02 11:49:38 -0800

    Card class now has more attributes that a card might have. really just a test.

--- src/Filters/Card.java
+++ src/Filters/Card.java
@@ -3,8 +3,20 @@ package Filters;
 import java.awt.geom.Point2D;
 
 public class Card {
+
+    public enum Color {
+        RED, GREEN, PURPLE
+    }
+    public enum Shape {
+        ROUNDEDRECT, DIAMOND, BEAN
+    }
+
     private Point2D tlCorner, trCorner, blCorner, brCorner, center;
     private int area;
+    private int shapes;
+
+    private Shape shape;
+    private Color color;
 
     public Card(Point2D center, int area) {
         this.center = center;
@@ -39,4 +51,22 @@ public class Card {
     public void setCenter(Point2D center) {
         this.center = center;
     }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
+    }
+
+
 }
